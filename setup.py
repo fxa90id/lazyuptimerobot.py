f3b8c7d64326884c3ec96e1c6fb22994dad15183
Michał Frontczak <michaljev@o2.pl>
2019-08-25 16:20:05 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lazyuptimerobot.py",
-    version="1.0.3",
+    version="2.0.0",
     author="Michal Frontczak",
     author_email="michaljev@o2.pl",
-    description="API Client for uptimerobot.com",
+    description="Python API Client for uptimerobot.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fxa90id/lazyuptimerobot.py",
@@ -19,7 +19,7 @@ setuptools.setup(
     python_requires='>=2.7',
     license="LGPLv3+",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
